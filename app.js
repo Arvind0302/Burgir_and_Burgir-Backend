3d1d8c4947202a93d44560697e1d89e820fa13f9
Arvind0302 <arvind.yadav.mec20@itbhu.ac.in>
2022-12-10 15:33:21 +0530

    google auth done with error handling

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 import  express from "express";
 import dotenv from "dotenv";
-
+import {connectPassport} from "./utils/Provider.js"
+import session from "express-session";
+import passport from "passport";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/user.js";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 const app = express();
 export default app;
 
 dotenv.config({
   path: "./config/config.env",
 });
+app.use(session({
+  secret:process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized:false,
+}))
+app.use(cookieParser());
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+connectPassport();
 
-import userRoute from "./routes/user.js";
 
 app.use("/api/v1", userRoute);
+
+app.use(errorMiddleware);
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,26 @@
 import express from "express";
 import passport from "passport";
-
+import { myProfile } from "../controllers/user.js";
+import { logout } from "../controllers/user.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
+
+router.get(
+    "/googlelogin",
+    passport.authenticate("google", {
+      scope: ["profile"],
+    })
+  );
+
+router.get(
+    "/login",
+    passport.authenticate("google", {
+      successRedirect: process.env.FRONTEND_URL,
+    })
+  );
+
+router.get("/me", isAuthenticated ,myProfile);
+
+router.get("/logout", logout);
 
 export default router;
